dcde3034c49edd86089c852aca494143dc111e19
Tasya Chandra <tasyachnd@gmail.com>
2021-04-26 13:10:40 +0700

    button + view UI udah beres, tinggal UIImage sama sesuaikan warna

--- Travelact/ConversationViewController.swift
+++ Travelact/ConversationViewController.swift
@@ -14,14 +14,24 @@ class ConversationViewController: UIViewController {
     @IBOutlet weak var answer1Button: UIButton!
     @IBOutlet weak var answer2Button: UIButton!
     @IBOutlet weak var answer3Button: UIButton!
+    @IBOutlet weak var replySelection: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        buttonUI()
+        //replySelection.isHidden = true
     }
     
     
-
+    func buttonUI(){
+        replySelection.layer.cornerRadius = 15
+        answer1Button.layer.cornerRadius = 10
+        answer2Button.layer.cornerRadius = 10
+        answer3Button.layer.cornerRadius = 10
+    }
 }
+
