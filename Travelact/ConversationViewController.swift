ee2d24d4893de7eaedaf8c21e8de6b912cad6019
Tasya Chandra <tasyachnd@gmail.com>
2021-04-26 11:57:11 +0700

    Conversation View Controller + button outlets

---
+++ Travelact/ConversationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ConversationViewController.swift
+//  Travelact
+//
+//  Created by Tasya Chandra on 26/04/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ConversationViewController: UIViewController {
+    
+    
+    @IBOutlet weak var answer1Button: UIButton!
+    @IBOutlet weak var answer2Button: UIButton!
+    @IBOutlet weak var answer3Button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
