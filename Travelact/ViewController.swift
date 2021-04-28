20b6251f640b5d4e32a17fa4b65b250c1fb16e47
martinusgalih <martinusgalih98@gmail.com>
2021-04-28 23:59:34 +0700

    fixed dismis keyboard

--- Travelact/ViewController.swift
+++ Travelact/ViewController.swift
@@ -17,8 +17,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var locLabel: UILabel!
     @IBOutlet weak var scrollView: UIPageControl!
-    
-    
+   
     @IBAction func startButton(_ sender: Any) {
         if bgImage.image == UIImage(named: "Beach") && inputName.text != "" {
            
@@ -51,7 +50,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             pageControl.currentPage = 2
             self.clickSound()
         } else if bgImage.image == UIImage(named: "comingsoon") {
-            titleLabel.text = "Hive Five Beach"
+            titleLabel.text = "Hive Beach Cafe"
             locLabel.text = "England"
             bgImage.image = UIImage(named: "Beach")
             pageControl.currentPage = 0
@@ -61,7 +60,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     @IBAction func backButton(_ sender: Any) {
         if bgImage.image == UIImage(named: "comingsoon") {
-            titleLabel.text = "Hive Five Beach"
+            titleLabel.text = "Hive Beach Cafe"
             locLabel.text = "England"
             bgImage.image = UIImage(named: "Beach")
             pageControl.currentPage = 0
@@ -82,8 +81,36 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
         pageControl.numberOfPages = 2
         UISetup()
+        
+        do {
+            try audioSession.setActive(true)
+            volume = audioSession.outputVolume
+        } catch {
+            print("Error Setting Up Audio Session")
+        }
+        //dismiss keyboard
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+            view.addGestureRecognizer(tap)
+        
+        //input on keyboard
+       
+        
     }
-
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+           // if keyboard size is not available for some reason, dont do anything
+           return
+        }
+        self.view.frame.origin.y = 0 - keyboardSize.height
+    }
+    
+    
+    
+    //dismiss keyboard
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
     //tampilan
     func UISetup() {
         startButton1.layer.cornerRadius = 27.5
@@ -100,8 +127,16 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         bgImage.image = UIImage(named: "Beach")
         locLabel.text = "England"
-        titleLabel.text = "Hive Five Beach"
+        titleLabel.text = "Hive Beach Cafe"
     }
+    
+    let vol = AVAudioSession.sharedInstance().outputVolume
+    let audioSession = AVAudioSession.sharedInstance()
+    var volume: Float?
+    
+    
+    
+    
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         if bgImage.image == UIImage(named: "Beach") && inputName.text != "" {
