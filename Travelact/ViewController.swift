36a129003915acc89b8021cd01a1e15b3178a2e5
martinusgalih <martinusgalih98@gmail.com>
2021-04-29 10:36:25 +0700

    keyboard input fixed

--- Travelact/ViewController.swift
+++ Travelact/ViewController.swift
@@ -7,7 +7,7 @@
 import AVFoundation
 import UIKit
 
-class ViewController: UIViewController, UIScrollViewDelegate {
+class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var bgPlane: UIView!
     @IBOutlet weak var bgImage: UIImageView!
@@ -77,9 +77,19 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
+            NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+          
+              // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
+            NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
         pageControl.numberOfPages = 2
         UISetup()
         bgSound()
+        
+        inputName.delegate = self
+        
         do {
             try audioSession.setActive(true)
             volume = audioSession.outputVolume
@@ -94,15 +104,23 @@ class ViewController: UIViewController, UIScrollViewDelegate {
        
         
     }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dismissKeyboard()
+        return true
+    }
+    
     @objc func keyboardWillShow(notification: NSNotification) {
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+        guard ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil else {
            // if keyboard size is not available for some reason, dont do anything
            return
         }
-        self.view.frame.origin.y = 0 - keyboardSize.height
+        self.view.frame.origin.y = 0 - 120
     }
     
-    
+    @objc func keyboardWillHide(notification: NSNotification) {
+      // move back the root view origin to zero
+      self.view.frame.origin.y = 0
+    }
     
     //dismiss keyboard
     @objc func dismissKeyboard() {
@@ -140,6 +158,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         if bgImage.image == UIImage(named: "Beach") && inputName.text != "" {
             bgSoundStop()
             return true
+    
         }  else {
              return false
         }
